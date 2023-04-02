a6eb9b786c754fa65a7e96374155954e04f23a1c
sadums <samjadams4@gmail.com>
2023-04-02 14:29:20 -0600

    cleaned up code and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,66 +28,18 @@ convertBtn.addEventListener('click', (event)=> {
    })
 
 
-/* GENERATE MAP DIV */
-var map;
-var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
-/* MODALS FOR MARKERS */
-// open modal
-var modalTitle = document.getElementById('modal-title');
-var modalBody = document.getElementById('modal-body');
-var modal = document.getElementById('modal');
-var showModal = function(event){
-  var clickedMarkerData = bankData.get(event.target); 
-  modalTitle.textContent = clickedMarkerData.name
-  modalBody.innerHTML = '';
-  if(clickedMarkerData.address != undefined && clickedMarkerData.address != null){
-    var addressElement = document.createElement("div");
-    addressElement.textContent = "Address: " + clickedMarkerData.address;
-    modalBody.prepend(addressElement);
-    var directionsButton = document.createElement("a");
-    directionsButton.setAttribute("href", `https://maps.google.com/?q=${clickedMarkerData.address}`);
-    directionsButton.setAttribute("target", "_blank");
-    directionsButton.textContent = "GOOGLE MAPS";
-    modalBody.append(directionsButton);
-  }
-  if(clickedMarkerData.phone != undefined && clickedMarkerData.phone != null){
-    var phoneElement = document.createElement("a");
-    phoneElement.setAttribute("href", "tel:" + clickedMarkerData.phone);
-    phoneElement.setAttribute("target", "_blank");
-    phoneElement.textContent = "Phone: " + clickedMarkerData.phone;
-    modalBody.prepend(phoneElement);
-  }
-  if(clickedMarkerData.url != undefined && clickedMarkerData.url != null){
-    var urlElement = document.createElement("a");
-    urlElement.setAttribute("href", "https://" + clickedMarkerData.url);
-    urlElement.setAttribute("target", "_blank");
-    urlElement.textContent = "Website: " + clickedMarkerData.url;
-    modalBody.prepend(urlElement);
-  }
-  modal.setAttribute('style', 'display: block;');
-}
-// close modal
-var exitModal = document.getElementById('modal-exit');
-exitModal.addEventListener("click", function(){
-  modal.setAttribute('style', 'display: none;');
-})
-
-/* PLACES API CALL */
+/* GENERATE MARKERS */
 var bankData = new Map();
-
-var search = "Bank";
-var limit = 20;
-var lat = 40.5086;
-var lon = -112.0125;
-var radius = 20000;
-
-var placesRequestURL = `https://api.tomtom.com/search/2/categorySearch/${search}.json?key=gzfilgZ8uJFWF9BCQHsE4daKJN4bToA9&limit=${limit}&lat=${lat}&lon=${lon}&radius=${radius}`
-
-var getPlaces = function(){
+var getPlaces = function(lat, lon){
+  // query criteria
+  var search = "Bank";
+  var limit = 20;
+  var radius = 20000;
+  // query
+  var placesRequestURL = `https://api.tomtom.com/search/2/categorySearch/${search}.json?key=gzfilgZ8uJFWF9BCQHsE4daKJN4bToA9&limit=${limit}&lat=${lat}&lon=${lon}&radius=${radius}`
+  // api call
   fetch(placesRequestURL)
   .then(response => response.json())
-  // Function for operating with result
   .then(function(result){
     console.log(result.results.length);
     for(var i = 0; i < result.results.length; i++){
@@ -109,25 +61,67 @@ var getPlaces = function(){
 }
 
 
-var testing = function(lat, lon){
+/* GENERATE MODALS FOR MARKERS */
+// modal html elements
+var modalTitle = document.getElementById('modal-title');
+var modalBody = document.getElementById('modal-body');
+var modal = document.getElementById('modal');
+var directionsButton = document.getElementById("modal-maps");
+var addressElement = document.getElementById("modal-address");
+var phoneElement = document.getElementById("modal-phone");
+var urlElement = document.getElementById("modal-website");
+
+var showModal = function(event){
+  var clickedMarkerData = bankData.get(event.target);
+  // diplay bank name
+  modalTitle.textContent = clickedMarkerData.name
+  // display address
+  if(clickedMarkerData.address != undefined && clickedMarkerData.address != null){
+    addressElement.textContent = "Address: " + clickedMarkerData.address;
+    directionsButton.setAttribute("href", `https://maps.google.com/?q=${clickedMarkerData.address}`);
+    directionsButton.setAttribute("target", "_blank");
+    directionsButton.textContent = "GOOGLE MAPS";
+  }
+  // display phone number
+  if(clickedMarkerData.phone != undefined && clickedMarkerData.phone != null){
+    phoneElement.setAttribute("href", "tel:" + clickedMarkerData.phone);
+    phoneElement.setAttribute("target", "_blank");
+    phoneElement.textContent = "Phone: " + clickedMarkerData.phone;
+  }
+  // display bank website
+  if(clickedMarkerData.url != undefined && clickedMarkerData.url != null){
+    urlElement.setAttribute("href", "https://" + clickedMarkerData.url);
+    urlElement.setAttribute("target", "_blank");
+    urlElement.textContent = "Website: " + clickedMarkerData.url;
+  }
+  modal.setAttribute('style', 'display: block;'); // show modal
+}
+// close modal
+var exitModal = document.getElementById('modal-exit');
+exitModal.addEventListener("click", function(){
+  modal.setAttribute('style', 'display: none;'); // hide modal
+});
+
+
+/* GENERATE MAP DIV */ 
+var map;
+var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var showMap = function(lat, lon){
   var mapOptions = {
     center:[lat, lon], //Where map will start
-    zoom:10
+    zoom:14 // map zoom
   }
   map = new L.map('map', mapOptions);
   map.addLayer(layer);
+  getPlaces(lat, lon);
 }
 
-const successCallback = (position) => {
-  testing(position.coords.latitude, position.coords.longitude);
-};
 
-const errorCallback = (error) => {
+/* ASK USER FOR LOCATION */ 
+const locationEnabled = (position) => {
+  showMap(position.coords.latitude, position.coords.longitude);
+};
+const locationDisabled = (error) => {
   console.log(error);
 };
-
-var testButton = document.getElementById("testingbutton");
-
-testButton.addEventListener("click", function(event){
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {timeout:10000});
-})
+// navigator.geolocation.getCurrentPosition(locationEnabled, locationDisabled, {timeout:10000});
